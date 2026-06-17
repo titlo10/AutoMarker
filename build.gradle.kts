e4e41aa04984fcf0897d488531770d204111e42d
titlo10 <titlo10rebooting@gmail.com>
2026-06-17 16:07:52 +0300

    Bump modVersion to 1.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ val mcRange = when (project.name) {
     "1.20.1" -> "1.20-1.20.1"
     else -> project.name
 }
-val modVersion = "1.0.1"
+val modVersion = "1.0.2"
 
 version = "${mcRange}-${modVersion}"
 base.archivesName.set("AutoMarker")
